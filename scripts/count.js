0346f18729934964d040cc288d19cfabf4422786
Eduard Prigoana <eduard@prigoana.com>
2025-06-28 12:14:04 +0300

    Update count.js

--- scripts/count.js
+++ scripts/count.js
@@ -7,12 +7,17 @@ async function fetchVisitorCount() {
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
 
     const data = await response.json();
-    const count = data.count;
+    const count = formatNumberWithCommas(data.count);
+
     document.getElementById("visitor-count").textContent = `You're visitor no. ${count}`;
   } catch (error) {
     document.getElementById("visitor-count").textContent = "";
     console.error("Error fetching visitor count:", error);
   }
+}
+
+function formatNumberWithCommas(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 fetchVisitorCount();
